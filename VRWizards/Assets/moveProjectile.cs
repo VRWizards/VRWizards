9ec487bd3880ee3459b20cc7da956de9c4ea8b70
gmarousek <graham.marousek@gmail.com>
2015-11-11 14:10:48 -0700

    Initial upload
    The VRWizards project, with a working projectile and some lava particles
    that come out of it!

---
+++ VRWizards/Assets/moveProjectile.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class moveProjectile : MonoBehaviour {
+
+	private Rigidbody rig;
+	public float force = 100000;
+	private Quaternion rot;
+	public HandModel hand;
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+
+		target = GameObject.Find ("Target").transform;
+
+	//	Quaternion lookAt = Quaternion.LookRotation(hand.GetPalmRotation() - transform.position);
+		//rot = Quaternion.FromToRotation(Vector3.forward, ;
+		rig = GetComponent<Rigidbody> ();
+		//rig.AddForce (hand.GetPalmRotation().eulerAngles * force);
+		rig.AddForce ((transform.position - target.transform.position) * force);
+		//rig.AddForce ((hand.GetPalmDirection()) * force);
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+
+		//transform.position = Vector3.MoveTowards(transform.position, target.position, force);
+	}
+}
